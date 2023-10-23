686dfc1d8e2d508eb3184de52ee659c0ba7759b2
nontapat123 <146747187+nontapat123@users.noreply.github.com>
2023-10-23 20:46:19 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GradingSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter score: ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int score))
+            {
+                Grading.GradeCalculator(score);
+            }
+            else
+            {
+                Console.WriteLine("Error: Score should be in digit only (" + input + ")");
+            }
+        }
+    }
+}
